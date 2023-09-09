803c4906881e85187c7d2f217c84ddf271a033a5
Tamotsu Kawano <t.k.kyoto@gmail.com>
2023-09-09 18:16:26 +0900

    Initial Commit

---
+++ TextInspector/TextInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInspectorApp.swift
+//  TextInspector
+//
+//  Created by 河野保 on 2023/09/09.
+//
+
+import SwiftUI
+
+@main
+struct TextInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
